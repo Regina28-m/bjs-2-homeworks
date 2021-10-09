21b3ade8ed1c0a27d5f87ee800601d6c5dd9f7d4
Regina28-m <regina-railan2013@yandex.ru>
2021-10-09 22:05:46 +0300

    first commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,23 @@
 function compareArrays(arr1, arr2) {
-  let result;
 
-  // Ваш код
+	// Ваш код
 
-  return result; // boolean
-}
+	let result = arr1.every((item, idx) => arr1.length === arr2.length && arr1[idx] === arr2[idx]);
+
+	return result;
+	return arr1.length === arr2.length && arr1.every((item, idx) => arr1[idx] === arr2[idx]);
+};
+
+
+
 
 function advancedFilter(arr) {
-  let resultArr;
+	let resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
 
-  // Ваш код
+	// Ваш код
 
-  return resultArr; // array
+	return resultArr; // array
+	return arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
+
+
 }
